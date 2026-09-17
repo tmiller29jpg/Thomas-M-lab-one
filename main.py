fe2d277c3f0890e31df93204b69623852ab6a7a1
tmiller29jpg <tmiller29@setoncatholic.org>
2026-09-17 21:41:14 +0000

    addded name age color

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
-name = input("Enter is your name")
-print ("Hello",name)
+name= input ("what is your name?")
+age= input ("what is your age?")
+color= input ("what is your favorite color?")
+print ("hello", name, "are you",age ,"years old and your favorite color is", color)
